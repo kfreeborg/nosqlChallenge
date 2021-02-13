771c8880fb56d9070a0f062591c7bc7f9710a4d1
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-02-13 15:05:45 -0700

    user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const { User } = require("../models");
+const { User, Thought } = require("../models");
 
 const userController = {
   getAllUsers(req, res) {
@@ -8,6 +8,25 @@ const userController = {
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
+      });
+  },
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate([
+        { path: "thoughts", select: "-__v" },
+        { path: "friends", select: "-__v" },
+      ])
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
       });
   },
 };
--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,4 @@
 const { Schema, model, Types } = require("mongoose");
-const User = require("./User");
 const moment = require("moment");
 
 const ReactionSchema = new Schema(
