64ee8952d3e9738d04e0ffafdf83251a40b511eb
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-02-12 20:04:40 -0700

    user model added

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { ObjectId } = require("bson");
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: "Please enter a username",
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
+    thoughts: [
+      {
+        type: ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
